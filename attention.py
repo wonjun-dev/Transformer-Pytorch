5daed379658d203cdaf59ab41f8eabf9ffc84ec8
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2022-04-30 22:10:37 +0900

    Add scale_dot_product_attention

--- attention.py
+++ attention.py
@@ -1,10 +1,32 @@
 # attention functions
+import math
+import torch
+import torch.nn.functional as F
 
-def scale_dot_product_attention():
-    pass
+
+def scale_dot_product_attention(Q, K, V):
+    # input Q, K, V -> (N, S, E)
+    # output QK^TV -> (N, S, E)
+    scale = math.sqrt(K.size()[-1])
+    scaled_weight = torch.matmul(Q, K.transpose(2, 1).contiguous()) / scale    # (QK^T)/scale -> (N, S, S), transpose 결과 contiguous 하지 않음.
+    softmax = F.softmax(scaled_weight.reshape(scaled_weight.size(0), -1), dim=1).view_as(scaled_weight) # N개의 (S, S) softmax 
+    attention = torch.matmul(softmax, V) 
+
+    return attention
+    
 
 def multi_head_attention():
     pass
 
 def encoder_decoder_attention():
     pass
+
+
+if __name__ == '__main__':
+    Q = torch.randn(2, 5, 10)
+    K = torch.randn(2, 5, 10)
+    V = torch.randn(2, 5, 10)
+
+
+    attention = scale_dot_product_attention(Q, K, V)
+    print(attention.size())
