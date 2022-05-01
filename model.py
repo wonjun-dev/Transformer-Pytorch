5755e9098a7743aec7e1e0cd81bbcfb9bc44ccbb
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2022-05-01 17:24:29 +0900

    Add positional encoding in forward

--- model.py
+++ model.py
@@ -4,14 +4,17 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 
 from module import Encoder, Decoder
+from positional_encoding import PositionalEncoding
 
 class Transformer(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, max_len, d_model):
         super().__init__()
-        self.encoder = Encoder()
+        self.pe = PositionalEncoding(max_len, d_model).encoding()
+        self.encoder = Encoder(d_model=d_model)
         self.decoder = Decoder()
 
     def forward(self, x):
+        x = x + self.pe
         x = self.encoder(x)
         # x = self.decoder(x)
         return x
