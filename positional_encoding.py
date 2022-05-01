5f474c36d4c9c1bb32ee783c03e412ae3a24f6d6
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2022-05-01 17:01:27 +0900

    Add positional encoding class

--- positional_encoding.py
+++ positional_encoding.py
@@ -1,7 +1,24 @@
-# positional encoding functions
+# positional encoding class
+import torch
 
-def sin_encoding():
-    pass
+class PositionalEncoding():
+    def __init__(self, max_len, d_model):
+        self.max_len = max_len
+        self.d_model = d_model
+        self.pe = self.encoding()
 
-def cos_encoding():
-    pass
+    def encoding(self):
+        pe = torch.zeros(self.max_len, self.d_model)
+        position = torch.arange(self.max_len).unsqueeze(1)
+        exponent = torch.arange(0, self.d_model, 2) / self.d_model
+        div = torch.pow(10000*torch.ones_like(exponent), exponent)
+        pe[:, ::2] = torch.sin(position / div)
+        pe[:, 1::2] = torch.cos(position / div)
+
+        return pe
+
+
+if __name__ == '__main__':
+    p = PositionalEncoding(10, 128)
+    print(p.pe.size())
+    print(p.pe)
