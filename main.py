b9f5900e69b9bb14461d6aa467c254f2ac038ee1
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2022-05-16 02:16:05 +0900

    Add embedding layers

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+from multiprocessing import dummy
 from torchtext.datasets import Multi30k
 import pytorch_lightning as pl
 
 # dummy input
 import torch
+from torch.utils.data import DataLoader
 
 from model import Transformer
+from data_manager import ToSentencePiece
 
 def main():
-    # train_iter, valid_iter, test_iter = Multi30k()
-    dummy_src = torch.rand((32, 10, 512)) # N, S, E
-    dummy_tgt = torch.rand((32, 10, 512))   
+    train_iter = Multi30k(split='train', language_pair=('en', 'de'))
+    train_dataloader = DataLoader(train_iter, batch_size=10, collate_fn=ToSentencePiece())
+    dummy_src, dummy_tgt = next(iter(train_dataloader))
+    # dummy_src = torch.rand((32, 10, 512)) # N, S, E
+    # dummy_tgt = torch.rand((32, 10, 512))   
 
-    transformer = Transformer(max_len=10, d_model=512, vocab_size=1000)
+    transformer = Transformer(max_len=200, d_model=512, vocab_size=5000)
+
     out = transformer(dummy_src, dummy_tgt)
     print(out.size())
     # trainer = pl.Trainer()
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import math
 from torch import nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
@@ -8,16 +9,21 @@ from positional_encoding import PositionalEncoding
 class Transformer(pl.LightningModule):
     def __init__(self, max_len, d_model, vocab_size):
         super().__init__()
-        # TODO Embedding layer
+        self.d_model = d_model
+        self.src_embedding = nn.Embedding(vocab_size, d_model)  # TODO src, tgt vocab 사이즈 따로
+        self.tgt_embedding = nn.Embedding(vocab_size, d_model)
         self.pe = PositionalEncoding(max_len, d_model).encoding()
         self.encoder = Encoder(d_model=d_model)
         self.decoder = Decoder(d_model=d_model)
         self.linear = nn.Linear(d_model, vocab_size)
 
     def forward(self, x, tgt):
-        x = x + self.pe
+        x = self.src_embedding(x.long()) * math.sqrt(self.d_model)
+        x = x + self.pe[:, :x.size()[1], :]
         x = self.encoder(x)
-        tgt = tgt + self.pe
+        
+        tgt = self.tgt_embedding(tgt.long()) * math.sqrt(self.d_model)
+        tgt = tgt + self.pe[:, :x.size()[1], :]
         x = self.decoder(tgt, x)
         x = self.linear(x)
         x = F.softmax(x, dim=-1)
